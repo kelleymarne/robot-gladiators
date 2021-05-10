8afcd898f41b2370a94d76780ba4d2cc6d916fb0
Kelley Worthington <kelleymarne@gmail.com>
2021-05-09 19:29:16 -0600

    shop function

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -92,6 +92,17 @@ for (var i = 0; i < enemyNames.length; i++) {
 
         // pass the pickedEnemyName variables vaule into the fight function, where it will assume the value of the enemyName parameter
         fight(pickedEnemyName);
+
+        //if we're not at the last enemy in the array
+        if (playerHealth > 0 && i < enemyNames.length -1) {
+        // ask if player wants to use the store before next round
+        var storeConfirm = window.confirm("The fight is over. Visit the store before the next round?");
+        //if yes take them to the store() function
+        if (storeConfirm) {
+         shop();
+        }
+    }
+
     } else {
         window.alert("You have lost your robot in battle! Game Over!");
         break;
@@ -117,5 +128,50 @@ var endGame = function() {
     }
 
 }
+
+//shop function
+var shop = function() {
+    // ask player what they'd like to do
+    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+    //use switch to carry out action
+    switch (shopOptionPrompt) {
+        case "REFILL":
+        case "Refill":
+        case "refill":
+            if(playerMoney >= 7){
+            window.alert("Refilling players health by 20 for 7 dollars.");
+            playerHealth = playerHealth + 20;
+            playerMoney = playerMoney - 7;
+        } else {
+            window.alert("You don't have enough money!");
+        }
+        break;
+        
+        case "upgrade":
+        case "Upgrade":
+        case "UPGRADE":
+            if(playerMoney >= 7) {
+            window.alert("Upgrading player's attack by 6 for 7 dollars.");
+            playerAttack = playerAttack + 6;
+            playerMoney = playerMoney - 7;
+        } else {
+            window.alert("Upgrading player's attack by 6 for 7 dollars."); 
+        }
+        break;
+
+        case "leave":
+        case "Leave":
+        case "LEAVE":
+            window.alert("Leaving the store."); 
+        break;
+
+        default:
+            window.alert("You did not pick a valid option. Try again!");
+            //call the store function again to force player to choose
+            shop();
+            break;
+        }   
+}
+
 
 startGame();
