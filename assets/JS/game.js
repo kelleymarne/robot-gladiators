f4aadd41c3329d654b46a27af6fa20532344c1a2
Kelley Worthington <kelleymarne@gmail.com>
2021-05-10 20:44:59 -0600

    Fixed Blank name response

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -79,8 +79,7 @@ var startGame = function () {
 
 for (var i = 0; i < enemyInfo.length; i++) {
     if(playerInfo.health > 0) {
-        window.alert("Welcome to Battlebots! Round " + (i + 1))
-        
+        window.alert("Welcome to Battlebots! Round " + (i + 1));
         // pick new enemy to fight based on the index of the enemyNames array
         var pickedEnemyObj = enemyInfo[i];
 
@@ -160,9 +159,21 @@ var shop = function() {
             break;
         }   
 }
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+    // ADD LOOP HERE//
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
